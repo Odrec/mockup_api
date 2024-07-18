cd494e4d604eedb3eb659e4db87c912051c34bf6
Dennis Benz <dbenz@uos.de>
2024-07-18 09:20:36 +0200

    Add typing for scope and reset interval

--- schemas.py
+++ schemas.py
@@ -1,12 +1,16 @@
 from pydantic import BaseModel
-from typing import List, Optional, Dict
+from typing import List, Optional, Dict, Literal, TypeAlias
+
+
+QuotaScope: TypeAlias = Literal["user", "course", "course-user", "total"]
+ResetIntervalDefinition: TypeAlias = Literal["daily", "weekly", "monthly", "semester"]
 
 
 class QuotaDefinition(BaseModel):
     type: Optional[str]
     description: Dict[str, str]
-    reset_interval: Optional[str]
-    scope: str
+    reset_interval: Optional[ResetIntervalDefinition]
+    scope: QuotaScope
     feature: Optional[str] = None
 
 
@@ -14,14 +18,14 @@ class QuotaGet(BaseModel):
     limit: int
     used: Optional[int] = None
     type: Optional[str]
-    scope: str
+    scope: QuotaScope
     feature: Optional[str] = None
     user_id: Optional[str] = None
 
 
 class QuotaUpdate(BaseModel):
     limit: int
-    scope: str
+    scope: QuotaScope
     feature: Optional[str] = None
     user_id: Optional[str] = None
 
